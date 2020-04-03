30fb0a2cae3d728de2ea7e4d566a11644eb8f3f3
nsgbs <56990557+nsgbs@users.noreply.github.com>
2020-04-03 17:27:11 -0300

    Correção de parâmetros

--- telemetria/value_setup.h
+++ telemetria/value_setup.h
@@ -1,6 +1,8 @@
 #define SSIDVALUE "" //SSID da rede
-#define PASSVALUE "" //PASSWD da rede
 #define VACVALUE 000000 //ID do canal de valores atuais
 #define VMCVVALUE 000000 //ID do canal de valores medios
 #define APIAVALUE "" //API de escrita de valores atuais
 #define APIMVALUE "" //API de escrita de valores medios
+#define EAP_IDENTITY "" // Login da rede
+#define EAP_PASSWORD "" // Password
+#define EAP_ANONYMOUS_IDENTITY "" // Valor geralmente vazio
