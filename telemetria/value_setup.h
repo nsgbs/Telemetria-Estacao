26ccdbe7780047eabaf4dad057fcdddfa75f9e12
nsgbs <56990557+nsgbs@users.noreply.github.com>
2020-04-03 17:24:10 -0300

    Adição do arquivo de variáveis

---
+++ telemetria/value_setup.h
@@ -0,0 +1,6 @@
+#define SSIDVALUE "" //SSID da rede
+#define PASSVALUE "" //PASSWD da rede
+#define VACVALUE 000000 //ID do canal de valores atuais
+#define VMCVVALUE 000000 //ID do canal de valores medios
+#define APIAVALUE "" //API de escrita de valores atuais
+#define APIMVALUE "" //API de escrita de valores medios
